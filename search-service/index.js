b6608a360c84c2883cea80a57c72f963fd7403c4
wilsonm14 <wilson.men0114@gmail.com>
2026-05-11 18:33:41 -0500

    Update index.js

--- search-service/index.js
+++ search-service/index.js
@@ -8,8 +8,9 @@ app.get('/search', async (req, res) => {
   try {
     const { name, category } = req.query;
 
+
     // llamar al business-service
-    const response = await axios.get('http://business-service:3000/business');
+    const response = await axios.get('http://host.docker.internal:3002/business');
    
     let results = response.data;
 
